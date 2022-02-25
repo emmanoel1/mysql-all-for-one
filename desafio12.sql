e80302470460451753ef2ad0f0a0220b75b2c7e6
emmanoel <emmanoelporto@gmail.com>
2022-02-25 17:09:01 -0300

    fix: req12

--- desafio12.sql
+++ desafio12.sql
@@ -1,3 +1,4 @@
 SELECT submitted_date FROM northwind.purchase_orders
-WHERE MONTH(submitted_date) = 04
-AND YEAR(submitted_date) = 2006;
+WHERE YEAR(submitted_date) = 2006
+AND MONTH(submitted_date) = 04
+AND DAY(submitted_date) = 26;
